6543430d2adcce922f23ab76e4e22ee5f79956e2
091nAnjar <oding.ginanjar@gmail.com>
2020-09-14 14:24:24 +0700

    Wallboard Zurich

---
+++ wb.project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wb.project.MvcApplication" Language="C#" %>
---
+++ wb.project/Models/runningTextModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wb.project.Models
+{
+    public class runningTextModel
+    {
+        public int runningTextSeqCd { get; set; }
+        public string runningText { get; set; }
+    }
+}
